47c45362b27c575f64bbc97cc386a296c766ab25
John Hiner <jrwenceslaus@gmail.com>
2016-09-28 16:56:40 -0400

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D myBody;
+    public float speed = 0.2f;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myBody.velocity = myBody.velocity + Vector2.left * speed; ;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myBody.velocity = myBody.velocity + Vector2.right * speed; ;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.velocity = -myBody.velocity;
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("HI!");
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("HI!");
+    }
+
+
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    Rigidbody2D myBody;
+
+    Vector2 movement;
+    public float speed = 4f;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        if (Random.value < 0.5f)
+            movement = Vector2.left * speed;
+        else
+            movement = Vector2.right * speed;
+    }
+
+	// Use this for initialization
+	void Start () {
+        myBody.velocity = movement;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("bingo");
+        if (col.gameObject.GetComponent<Player>() == null) {
+            if (movement == Vector2.left * speed)
+                movement = Vector2.right * speed;
+            else
+                movement = Vector2.left * speed;
+
+            myBody.velocity = movement;
+        }
+    }
+}
